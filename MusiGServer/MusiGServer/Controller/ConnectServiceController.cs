c9229b49a35c28127ed83993c9b632c8ac141ab3
Jonas Wanninger <jonas@ehvag.de>
2017-01-12 22:28:42 +0100

    wip

---
+++ MusiGServer/MusiGServer/Controller/ConnectServiceController.cs
@@ -0,0 +1,18 @@
+﻿using StatsHelix.Charizard;
+
+namespace MusiGServer.Controller
+{
+    [Controller]
+    class ConnectServiceController : IConnectService
+    {
+        public void Google()
+        {
+
+        }
+
+        public void Soundcloud()
+        {
+
+        }
+    }
+}
---
+++ MusiGServer/MusiGServer/Controller/OAuth2CallbackController.cs
@@ -0,0 +1,12 @@
+﻿using StatsHelix.Charizard;
+using static StatsHelix.Charizard.HttpResponse;
+using System.Threading.Tasks;
+
+namespace MusiGServer.Controller
+{
+    [Controller]
+    class OAuth2CallbackController
+    {
+
+    }
+}
